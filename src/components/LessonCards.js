6a5f12af17a371ddd4b471983d7c9f582a225419
unknown <egecan18k@gmail.com>
2020-10-17 11:52:28 -0400

    design up

--- src/components/LessonCards.js
+++ src/components/LessonCards.js
@@ -44,13 +44,43 @@ export default class LessonCards extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
+        <section
+          id="postsGrid"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            gridGap: "1em",
+            marginLeft: "5%",
+            marginTop: "5%"
+          }}
+        >
+       
           {items.map(item => (
-          <li key={item.name}>
-         {item.name}
-          </li>
+            <div key={item.name}>
+                  <div class="card" style={{ width: "18rem" }}>
+          <img
+            style={{ width: "18rem", height: "200px" }}
+            src="https://images.unsplash.com/photo-1602354949094-d4a7286c8f6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            class="card-img-top"
+            alt="..."
+          />
+
+          <div class="card-body">
+          <h5 class="card-title">{item.name}</h5>
+            <p class="card-text">
+           {item.description}
+            </p>
+
+           <p  style={{ color: "blue"}}> {item.category.category_en}</p> 
+          </div>
+        </div>
+            </div>
+        //   <li key={item.name}>
+        //  {item.name}
+        //   </li>
         ))}
-        </ul>
+     
+        </section>
       );
     }
   }
