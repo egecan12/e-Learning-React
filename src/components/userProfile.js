6e7dba0702ee62b4e04eac311fb1f3c002f0decf
egecan kahyaoglu <egecan18k@gmail.com>
2020-11-08 18:35:11 -0500

    userProfile dynamic name

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,46 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 export default class userProfile extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wYWxhbWFuYS5jb20iLCJhdWQiOiJodHRwOlwvXC9wYWxhbWFuYS5jb20iLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6eyJpZCI6IjI0IiwiZmlyc3RuYW1lIjoiIiwidXNlcm5hbWUiOiJlZ2VjYW4iLCJsYXN0bmFtZSI6IiIsImVtYWlsIjoiZWdlY2FuQGVnZWNhbi5jb20ifX0.vzZx6D5rU5WgD6L6VxP6K9c3bmRKnQxwwMaItpUvjWs",
-        };
-      }
-  componentDidMount() {
-          if (!localStorage.token) {
-      return window.location.pathname = "/401";
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      jwt: localStorage.token,
+      settings: null,
+    };
+  }
+  componentDidMount() {
+    if (!localStorage.token) {
+      return (window.location.pathname = "/401");
     }
     axios
-    .post(
-      "https://cors-anywhere.herokuapp.com/http://teletagsdev-env.eba-a2qmj2nq.us-east-2.elasticbeanstalk.com/api/user/validate_token.php",
-      this.state
-    )
-    .then((response) => {
-      if (response.data.error) {
-        // return toast.error(response.data.message);
-        console.log(response.data.error);
+      .post(
+        "https://cors-anywhere.herokuapp.com/http://teletagsdev-env.eba-a2qmj2nq.us-east-2.elasticbeanstalk.com/api/user/validate_token.php",
+        this.state
+      )
+      .then((response) => {
+        if (response.data.error) {
+          // return toast.error(response.data.message);
+          console.log(response.data.error);
+        }
+        console.log(response.data);
+        this.setState({ settings: response.data }, () => {});
+        console.log(this.state.settings.data.username);
 
-      }
-      console.log(response.data);
-    //   localStorage.token = response.data.jwt;
-      // toast.success(response.data.message);
-
-    })
-    .catch((error) => {
-    //   toast.error("an error occured");
-    });
-   
+        //   localStorage.token = response.data.jwt;
+        // toast.success(response.data.message);
+      })
+      .catch((error) => {
+        //   toast.error("an error occured");
+      });
   }
   render() {
-
+    const { settings } = this.state; //statedeki settingi rendera aliyoruz
+    console.log(settings);
     return (
-      <div>
-        <h1>Welcome to User Profile Page</h1>
-      </div>
+      settings && (
+        <div>
+         <h1>Welcome {settings.data.username} </h1>
+        </div>
+      )
     );
   }
 }
