9988cac58cfbf588a8fc77003f2c38a7b00e0463
egecan kahyaoglu <egecan18k@gmail.com>
2020-11-08 13:32:33 -0500

    login input bootstrap

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 class login extends Component {
   constructor(props) {
     super(props);
@@ -25,16 +28,16 @@ class login extends Component {
       )
       .then((response) => {
         if (response.data.error) {
-          return console.log("error");
-
+          return toast.error(response.data.message);
         }
         console.log(response.data);
         localStorage.token = response.data.jwt;
-        window.location.pathname = "/userProfile";
+        // toast.success(response.data.message);
 
+        window.location.pathname = "/userProfile";
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("an error occured");
       });
   };
 
@@ -43,7 +46,104 @@ class login extends Component {
     const { email, password, accessToken } = this.state;
     return (
       <div>
-        <form onSubmit={this.submitHandler}>
+        <ToastContainer autoClose={2000} position="top-left" zIndex={99999} />
+
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          Sign-In
+        </button>
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Modal title
+                </h5>
+
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form onSubmit={this.submitHandler}>
+                  {" "}
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      name="email"
+                      value={email}
+                      onChange={this.changeHandler}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                      We'll never share your email with anyone else.
+                    </small>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="exampleInputPassword1"
+                      onChange={this.changeHandler}
+                      name="password"
+                      value={password}
+                    />
+                  </div>
+                  <div className="form-group form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="exampleCheck1"
+                    />
+                    <label className="form-check-label" htmlFor="exampleCheck1">
+                      Check me out
+                    </label>
+                  </div>
+                  <br />
+                  <p>No account yet? Go register!</p>
+                  <br />
+                  <button
+                    type="submit"
+                    className="btn btn-primary"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <form onSubmit={this.submitHandler}>
           <div>
             <input
               type="text"
@@ -70,7 +170,7 @@ class login extends Component {
             />
           </div>
           <button type="submit">Submit</button>
-        </form>
+        </form> */}
       </div>
     );
   }
